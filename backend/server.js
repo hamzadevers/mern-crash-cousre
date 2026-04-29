78d0419889b65d8151dd450343ee9321b43a8bea
Hamza Imran <hamzadev024@gmail.com>
2026-04-29 10:51:33 +0500

    Fix case sensitivity in model import

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,31 @@
-import dns from 'dns';
-import mongoose from 'mongoose';
-dns.setServers(["8.8.8.8","8.8.4.4"]);
-import Product from './models/Product.model.js';
-import express from 'express';
-import dotenv from 'dotenv';
-import path from 'path';
-import {connectDB} from './config/db.js';
-import productRoutes from './routes/product.route.js';
+import dns from "dns";
+import mongoose from "mongoose";
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+import Product from "product.model.js";
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/product.route.js";
 dotenv.config();
-
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
 const __dirname = path.resolve();
 
-app.use(express.json()); 
-app.use("/api/products",productRoutes);
+app.use(express.json());
+app.use("/api/products", productRoutes);
 
-if (process.env.NODE_ENV === "production"){
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-    app.get("*", (req, res)=>{
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-    });
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
 }
 
-
 app.listen(PORT, () => {
-    connectDB();
-    console.log("server started at http://localhost:" + PORT);
+  connectDB();
+  console.log("server started at http://localhost:" + PORT);
 });
