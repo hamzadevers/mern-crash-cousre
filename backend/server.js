d69faca8a5af9c5b7031e194b22e9ef6543be426
Hamza Imran <hamzadev024@gmail.com>
2026-04-29 10:56:36 +0500

    Clean up duplicate code in server file

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,28 @@
-import dns from "dns";
-import mongoose from "mongoose";
-dns.setServers(["8.8.8.8", "8.8.4.4"]);
-import Product from "product.model.js";
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
+import dns from "dns";
+
+// Fix for some Render MongoDB connection issues
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
+
+// Load environment variables
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-
 const __dirname = path.resolve();
 
+// Middleware
 app.use(express.json());
+
+// API Routes
 app.use("/api/products", productRoutes);
 
+// Deployment / Production setup for serving React
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
@@ -25,7 +31,8 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
+// Start Server & Connect to Database
 app.listen(PORT, () => {
   connectDB();
-  console.log("server started at http://localhost:" + PORT);
+  console.log("Server started at http://localhost:" + PORT);
 });
